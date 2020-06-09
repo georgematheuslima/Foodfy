05ee08c206985c4a32fc20687c691fb5ebbefe6e
George Matheus <geoorge.m@hotmail.comm.com>
2020-06-09 19:00:57 -0300

    First commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
 
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add("active")
+    })
+}
 
-
-
-document.querySelector('.close-modal').addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src = ""
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
 })
+
+modal
