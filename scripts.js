7e202030085e0694239f347d6a9f10d703c58f57
George Matheus <60715730+georgematheuslima@users.noreply.github.com>
2020-06-10 13:42:51 -0300

    updated

--- scripts.js
+++ scripts.js
@@ -4,11 +4,17 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener("click", function(){
         modalOverlay.classList.add("active")
+        const imgId = card.getAttribute('id');
+        const cardName = card.querySelector('.card__content').querySelector('p').textContent;
+        const madeBy = card.querySelector('.card__info').querySelector('p').textContent;
+        modalOverlay.querySelector('img').src = `/assets/${imgId}.png`;
+        modalOverlay.querySelector('#nome_receita').textContent = cardName;
+        modalOverlay.querySelector('#chef_receita').textContent = madeBy
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector('img').src= "";
 })
 
-modal
